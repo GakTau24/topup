8960d131b142055c11d471385f41038daf81fb46
GakTau24 <107782664+GakTau24@users.noreply.github.com>
2023-07-06 21:38:58 +0700

    Update Mobile.jsx

--- components/Mobile.jsx
+++ components/Mobile.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 async function getMobileGames() {
-    const res = await fetch(`${process.env.baseURL}/mobilegames`)
+    const res = await fetch(`${process.env.baseURL}/mobilegames`, { cache: 'no-store' })
     return res.json()
 }
 
